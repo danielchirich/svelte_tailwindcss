d4ea3295b39e09975596e569a3493a2fb09bdabb
danielchirich <daniel.chirich@gmail.com>
2021-06-10 16:41:20 -0300

    Update App.svelte
    El mismo Todo de antes pero optimizado para movil

--- src/App.svelte
+++ src/App.svelte
@@ -38,103 +38,100 @@
 
 <Tailwindcss />
 <main>
-<section class="text-gray-600 body-font"></section>
-  
-<div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
-  <div class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-left w-full mt-5 md:mt-0">
-	<h1>Detalle</h1>
-<br/>
+<div class="h-auto flex items-center justify-center">
+<div class="w-full md:w-1/3 bg-white rounded-lg items-center">
+<h1 class="text-3xl text-center text-gray-700 mb-4">Todo</h1>
+
+<div class="px-12 pb-10">
 {#each todos as todo}
-	<div class="relative flex-grow w-full">
+	
 		<input
 			type=checkbox
 			bind:checked={todo.done}
-			
+			class="" 
 		>
-    <br/>
-	    <input
+    <div class="w-full mb-2">
+       <div class="flex justify-center">
+           <input 
 		    type=date
 			placeholder="Nuevo Pago Fecha"
 			bind:value={todo.fecha}
 			disabled={todo.done}
-			class=" bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" 
-		>
-		<input
+            class="px-8 w-full border rounded py-2 text-gray-700 focus:outline-none" />
+       </div>
+    </div>
+
+	<div class="w-full mb-2">
+       <div class="flex justify-center">
+           <input 
 		    type=text
 			placeholder="Cliente Nombre"
 			bind:value={todo.textClient}
 			disabled={todo.done}
-			class=" bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" 
-		>
-		<input
+            class="px-8 w-full border rounded py-2 text-gray-700 focus:outline-none" />
+       </div>
+    </div>
+	
+	<div class="w-full mb-2">
+       <div class="flex justify-center">
+           <input 
 		    type=number
-			placeholder="Nuevo Pago Cantidad"
+			placeholder="Pago Cantidad"
 			bind:value={todo.numberCantidad}
 			disabled={todo.done}
-			class=" bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" 
-		>
-		<input
+            class="px-8 w-full border rounded py-2 text-gray-700 focus:outline-none" />
+       </div>
+    </div>    
+	
+	<div class="w-full mb-2">
+       <div class="flex justify-center">
+           <input 
 		    type=text
-			placeholder="Nuevo Pago Detalle"
+			placeholder="Descripción"
 			bind:value={todo.textDetalle}
 			disabled={todo.done}
-			class=" bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" 
-		>
-		<input
+            class="px-8 w-full border rounded py-2 text-gray-700 focus:outline-none" />
+       </div>
+    </div>	
+	<div class="w-full mb-2">
+       <div class="flex justify-center">
+            <input 
 		    type=number
-			placeholder="Nuevo Pago Importe "
+			placeholder="Importe"
 			bind:value={todo.numberImporte}
 			disabled={todo.done}
-			class=" bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" 
-		>
-	</div>
-	
-	<br/>
+            class="px-8 w-full border rounded py-2 text-gray-700 focus:outline-none" />
+       </div>
+    </div>  	
+				
 {/each}
-<br/>
-<button on:click={add} class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+
+<button on:click={add} class="w-full mt-6 py-2  rounded bg-blue-500 text-gray-100  focus:outline-none ">
 	Agregar Nuevo
 </button>
-<br/>
-<button on:click={clear} class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+
+<button on:click={clear} class="w-full mt-6 py-2  rounded bg-blue-500 text-gray-100  focus:outline-none">
 	Quitar Pagado
 </button>
-<br/>
+
+</div>
+
 {#each todosSort as todoSort,i}
 		<ul>
-		  <li class="lg:w-1/3 mb-1 w-1/2">
+		    <li class="">
 		                {i+1}) 
 		                {todoSort.fecha}
 						{todoSort.textClient} 
 						{todoSort.numberImporte}
 
-			</li>			
+		    </li>			
 		</ul>
-	{/each}
+{/each}
 </div>
 </div>
-
-
-
-
 </main>
 
- <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-	h1 {
-		color: #ff3e00;
-		
-		font-size: 2em;
-		font-weight: 100;
-	}
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style> 
+
+
+
+ 
